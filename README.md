31abb31f6a67272cd7bca5db716d0cd854588d33
moriyuto <ymori0323@gmail.com>
2024-05-07 06:56:26 +0900

    アシスタントAPI

--- README.md
+++ README.md
@@ -17,7 +17,10 @@ response = service.chat_completion("gpt-3.5-turbo", [
 ])
 
 * ファイルアップロード  
-dataset_response = service.upload_dataset('lib/data/tuning002.jsonl')
+response = service.upload_file('lib/data/tuning002.jsonl', 'fine-tune')
+
+※ アシスタントに使うためには
+response = service.upload_file('lib/data/tuning002.jsonl', 'assistants')
 
 * ジョブ作成  
 job_response = service.create_fine_tuning_job('file-gMwqsc5JLs9ApEj09uHmJwzr', 'gpt-3.5-turbo')  
@@ -28,3 +31,24 @@ job_status = service.retrieve_fine_tuning_job('ftjob-NBXWIsdKFmX9O8hc19sqyCEg')
 ※ジョブ作成時のidを入力する  
 statusがsucceededになったら、fine_tuned_modelに記載された文字列をメモし
 テキスト生成のモデルに指定する
+
+* アシスタント作成
+response = service.create_assistant("gpt-3.5-turbo", "カスタマーサポート２", "ユーザーからの問い合わせに対して返信メッセージを作成します", "file_search", ["vs_izYRAWyFXEWX5eejoQN9mpVc"])
+
+
+vs_izYRAWyFXEWX5eejoQN9mpVc
+
+* スレッド作成
+response = service.create_thread
+
+thread_McgWYbkVuG1TkRbxsK3ejNU5
+
+* メッセージをスレッドに付与
+response = service.create_message_to_thread("thread_7ygZRc6PW2ZfcnYO1nJRXjZq", "user", "処方せんはどのくらいの時間で発行され、薬局で薬を受け取れるようになりますか？")
+
+* スレッドを実行
+response = service.thread_runs("thread_7ygZRc6PW2ZfcnYO1nJRXjZq", "asst_VoQ5BG5OYbYlyoeWNGsPcOYi")
+
+* メッセージの取得
+response = service.get_messages("thread_7ygZRc6PW2ZfcnYO1nJRXjZq")
+
--- app/services/open_ai_chat_service.rb
+++ app/services/open_ai_chat_service.rb
@@ -11,6 +11,11 @@ class OpenAiChatService
       "Content-Type" => "application/json",
       "Authorization" => "Bearer #{api_key}"
     }
+    @beta_headers = {
+      "Content-Type" => "application/json",
+      "Authorization" => "Bearer #{api_key}",
+      "OpenAI-Beta" => "assistants=v2"
+    }
     @base_uri = 'https://api.openai.com/v1'
   end
 
@@ -24,28 +29,37 @@ class OpenAiChatService
     puts response
   end
 
-  def upload_dataset(file_path)
+  def upload_file(file_path, purpose)
     response = HTTParty.post("#{@base_uri}/files",
       headers: @headers,
       body: {
         file: File.open(file_path, "r"),
-        purpose: "fine-tune"
+        purpose: purpose
       },
       multipart: true
     )
     puts response
   end
 
-  def create_fine_tuning_job(training_file_id, model)
+  def create_fine_tuning_job(training_file_id, model, epochs=3)
     response = HTTParty.post("#{@base_uri}/fine_tuning/jobs",
       headers: @headers,
       body: {
         training_file: training_file_id,
-        model: model
+        model: model,
       }.to_json
     )
     puts response
   end
+
+  # エポックを指定する場合 メモ
+  # body: {
+  #   training_file: training_file_id,
+  #   model: model,
+  #   hyperparameters: {
+  #     n_epochs: epochs
+  #   }
+  # }.to_json
 
   def retrieve_fine_tuning_job(job_id)
     response = HTTParty.get("#{@base_uri}/fine_tuning/jobs/#{job_id}",
@@ -53,4 +67,112 @@ class OpenAiChatService
     )
     puts response
   end
+
+  # ファインチューニングジョブのリストを取得するメソッド
+  def list_fine_tuning_jobs(limit = 10)
+    response = HTTParty.get(
+      "#{@base_uri}/fine_tuning/jobs",
+      headers: @headers,
+      query: {
+        limit: limit
+      }
+    )
+    
+    if response.code == 200
+      jobs = response.parsed_response["data"]
+      jobs.each do |job|
+        puts "Job ID: #{job['id']}, Status: #{job['status']}"
+      end
+    else
+      puts "Failed to retrieve fine-tuning jobs: #{response.message}"
+    end
+  end
+
+  def get_file_content(file_id)
+    response = HTTParty.get(
+      "#{@base_uri}/files/#{file_id}/content",
+      headers: @headers
+    )
+  
+    utf8_content = response.body.force_encoding('UTF-8')
+    puts utf8_content
+    
+  end
+
+  # code_interpreter用
+  # def create_assistant(model, name, instructions, tools, files)
+  #   response = HTTParty.post("#{@base_uri}/assistants",
+  #     headers: @beta_headers,
+  #     body: {
+  #       model: model,
+  #       name: name,
+  #       instructions: instructions,
+  #       tools: [{type: tools}],
+  #       tool_resources: {
+  #         "code_interpreter": {
+  #           "file_ids": files
+  #         }
+  #       }
+  #     }.to_json
+  #   )
+  #   puts response
+  # end
+
+  # file_search用
+  def create_assistant(model, name, instructions, tools, files)
+    response = HTTParty.post("#{@base_uri}/assistants",
+      headers: @beta_headers,
+      body: {
+        model: model,
+        name: name,
+        instructions: instructions,
+        tools: [{type: tools}],
+        tool_resources: {
+          "file_search": {
+            "vector_store_ids": files
+          }
+        }
+      }.to_json
+    )
+    puts response
+  end
+
+  def create_thread
+    # body = {
+    #   assistant_id: assistant_id  # アシスタントIDをリクエストに含める
+    # }.to_json
+    response = HTTParty.post("#{@base_uri}/threads",
+      headers: @beta_headers,
+    )
+    puts response
+  end
+
+  def create_message_to_thread(thread_id, role, content)
+    response = HTTParty.post("#{@base_uri}/threads/#{thread_id}/messages",
+      headers: @beta_headers,
+      body: {
+        role: role,
+        content: content
+      }.to_json
+    )
+    puts response
+  end
+
+  def thread_runs(thread_id, assistant_id)
+    response = HTTParty.post("#{@base_uri}/threads/#{thread_id}/runs",
+      headers: @beta_headers,
+      body: {
+        assistant_id: assistant_id,
+      }.to_json
+    )
+    puts response
+  end
+
+  def get_messages(thread_id)
+    response = HTTParty.get("#{@base_uri}/threads/#{thread_id}/messages",
+      headers: @beta_headers,
+    )
+    puts response
+  end
+
 end
